8b010ed62a2bb61fd124968fceb4e471924a62b1
artemtarasov <tematarasov48rus@mail.ru>
2025-11-18 16:59:45 +0300

    [fix] Fix test import

--- shared/src/commonTest/kotlin/org/angryscan/app/searcher/FileTypeTest.kt
+++ shared/src/commonTest/kotlin/org/angryscan/app/searcher/FileTypeTest.kt
@@ -1,22 +1,24 @@
 package org.angryscan.app.searcher
 
-import org.apache.poi.openxml4j.util.ZipSecureFile
-import kotlinx.coroutines.*
-import org.angryscan.common.engine.IMatcher
-import org.angryscan.common.engine.hyperscan.HyperScanEngine
-import org.angryscan.common.engine.kotlin.KotlinEngine
-import org.angryscan.common.extensions.Matchers
-import org.junit.Rule
-import org.koin.dsl.module
-import org.koin.test.KoinTestRule
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.runBlocking
 import org.angryscan.app.common.AppSettings
 import org.angryscan.app.common.ScanSettings
 import org.angryscan.app.common.UserSignatureSettings
 import org.angryscan.app.db.DatabaseSettings
 import org.angryscan.app.di.scanModule
 import org.angryscan.app.scan.common.files.FileType
-import org.angryscan.app.scan.engine.toHyperScanMatchers
 import org.angryscan.app.scan.engine.toKotlinMatchers
+import org.angryscan.common.engine.IMatcher
+import org.angryscan.common.engine.hyperscan.HyperScanEngine
+import org.angryscan.common.engine.hyperscan.IHyperMatcher
+import org.angryscan.common.engine.kotlin.KotlinEngine
+import org.angryscan.common.extensions.Matchers
+import org.angryscan.common.matchers.*
+import org.apache.poi.openxml4j.util.ZipSecureFile
+import org.junit.Rule
+import org.koin.dsl.module
+import org.koin.test.KoinTestRule
 import java.io.File
 import java.io.FileWriter
 import kotlin.test.*
@@ -61,6 +63,23 @@ internal class FileTypeTest() {
 
     @Test
     fun `Check file types`() {
+        val targetMatchers = listOf<IHyperMatcher>(
+            Email,
+            CardNumber(),
+            Phone,
+            SNILS,
+            Passport,
+            OMS,
+            INN,
+            Address,
+            Login,
+            Password,
+            CVV,
+            FullName,
+            IPv4,
+            IPv6
+        )
+
         listOf(
             "1.docx",
             "emails_result.xlsx",
@@ -99,7 +118,7 @@ internal class FileTypeTest() {
                         enumType?.scanFile(
                             f,
                             currentCoroutineContext(),
-                            listOf(HyperScanEngine(Matchers.toHyperScanMatchers())),
+                            listOf(HyperScanEngine(targetMatchers)),
                             false
                         ).let { doc ->
                             Matrix.getMap(filename)
